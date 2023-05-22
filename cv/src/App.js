fc09fc622deb632db739cd98cac45d6ea46f1600
Elcan <eljanng@code.edu.az>
2023-05-22 23:06:12 +0400

    CV Homework

---
+++ cv/src/App.js
@@ -0,0 +1,243 @@
+import './App.css';
+
+function App() {
+  return (
+
+
+
+    <main>
+
+      <div className='container mt-5'>
+
+        <div className='header'>
+
+          <div className='gray'>
+
+            <div className='info'>
+
+              <h1>
+                Elcan Qurbanov
+              </h1>
+
+              <h5>
+                Full Stack Developer
+              </h5>
+
+            </div>
+
+          </div>
+
+          <div className='black'>
+
+          </div>
+
+        </div>
+
+
+        <div className='content'>
+
+          <div className='container'>
+
+            <div className='row'>
+
+              <div className='col-5'>
+
+                <div className='left'>
+
+                  <div className='contact mt-3'>
+
+                    <div>
+                      <h3>Contact</h3>
+                    </div>
+
+                    <ul>
+                      <li>Phone: +994-318-12-12</li>
+                      <li>Email: eljanng@code.edu.az</li>
+                      <li>Portfolio: www.linkedin.com/in/ElcanQurbanov</li>
+                      <li>Address: Ehmedli</li>
+                    </ul>
+
+                    <div>
+                      <hr></hr>
+                    </div>
+
+                  </div>
+
+                  <div className='skills mt-3'>
+
+                    <div>
+                      <h3>Skills</h3>
+                    </div>
+
+                    <ul>
+                      <li>Creavity</li>
+                      <li>Multi-tasking</li>
+                      <li>Software Developer</li>
+                    </ul>
+                    
+                    <div>
+                      <hr></hr>
+                    </div>
+
+                  </div>
+
+                  <div className='award mt-3'>
+
+                    <div>
+                      <h3>Awards</h3>
+                    </div>
+
+                    <ul>
+                      <li>Software Developer</li>
+                    </ul>
+
+                    <div>
+                      <hr></hr>
+                    </div>
+
+                  </div>
+
+                  <div className='language mt-3'>
+
+                    <div>
+                      <h3>Languages</h3>
+                    </div>
+
+                    <ul>
+                      <li>English</li>
+                      <li>Turkish</li>
+                    </ul>
+
+                    <div>
+                      <hr></hr>
+                    </div>
+
+                  </div>
+
+                  <div className='hobbies mt-3'>
+
+                    <div>
+                      <h3>Hobbies</h3>
+                    </div>
+
+                    <ul>
+                      <li>Codes</li>
+                      <li>Error fixed</li>
+                    </ul>
+
+                
+                  </div>
+
+                  {/* <div className='reference mt-3'>
+
+                    <div>
+                      <h3>References</h3>
+                    </div>
+
+                    <ul>
+                      <li>Email: eljanng@code.edu.az</li>
+                      <li>Portfolio: www.linkedin.com/in/ElcanQurbanov</li>
+                    </ul>
+
+                  </div> */}
+
+
+
+                </div>
+
+              </div>
+
+              <div className='col-7'>
+
+                <div className='right'>
+
+
+                  <div className='objective mt-3'>
+
+                    <div>
+                      <h3>Objective</h3>
+                    </div>
+
+                    <div>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia sed felis id sollicitudin. Fusce sodales erat eget rutrum scelerisque. Phasellus fermentum magna leo, at pellentesque nibh mattis vel. Mauris imperdiet erat a nunc rutrum elementum. Nullam non est eros. Vivamus pellentesque metus massa, non lacinia risus aliquam nec. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean laoreet leo in dignissim hendrerit. Cras suscipit massa eget lorem fringilla tempor. Donec in mi ornare, suscipit magna et, interdum lorem. Etiam egestas, metus vel vehicula varius, eros neque commodo dolor, sed tempor nisi nulla id justo. Ut gravida, mauris id commodo tristique, erat velit vulputate orci, ac viverra diam lectus vel nisl.
+                    </div>
+
+                    <div>
+                      <hr></hr>
+                    </div>
+
+                  </div>
+
+
+                </div>
+
+
+                <div className='education mt-3'>
+
+                  <div>
+                    <h3>Education</h3>
+                  </div>
+
+                  <ul>
+                    <li>Code Academy</li>
+                  </ul>
+
+                  <div>
+                      <hr></hr>
+                    </div>
+
+                </div>
+
+                <div className='experience mt-3'>
+
+                  <div>
+                    <h3>Experience</h3>
+                  </div>
+
+                  <ul>
+                    <li>Full-Stack Developer</li>
+                  </ul>
+
+                  <div>
+                      <hr></hr>
+                    </div>
+
+                </div>
+
+                <div className='certificate mt-3'>
+                    
+                    <div>
+                      <h3>Certificates</h3>
+                    </div>
+
+                    <ul>
+                      <li>Software Developer</li>
+                 
+                    </ul>
+
+                  </div>
+
+
+
+              </div>
+
+            </div>
+
+
+          </div>
+
+        </div>
+
+
+
+      </div>
+
+
+
+
+
+    </main>
+  );
+}
+
+export default App;
